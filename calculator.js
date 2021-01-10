c5b16227c3c53118764f62327cfbb3d1f827cf8a
hesham <hesham@heshams-iMac.local>
2021-01-10 17:04:07 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 // Define the numbers and the operation here
+let number1=prompt("Please enter first number");
+let number2=prompt("Please enter second number");
+const operation=prompt("Please enter operation");
 
 // Write if-statements here
+
+if (operation === '+') console.log(number1 + number2);
+else if (operation === '-') console.log(number1 - number2);
+else if (operation === '*') console.log(number1 * number2);
+else if (operation === '/') console.log(number1 / number2);
+else console.log("Error!!");
